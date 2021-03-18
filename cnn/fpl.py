22d67c2e09caba4eb5f079d6d3c1f68b87df1a55
Daan Krol <daankrol@icloud.com>
2021-03-18 16:25:14 +0100

    refactoring

--- cnn/fpl.py
+++ cnn/fpl.py
@@ -6,7 +6,7 @@ from tensorflow import keras
 from tensorflow.keras.applications.vgg19 import VGG19
 from tensorflow.keras import layers
 # from enum import Enum
-from layers import VGG_ReLu_Layer
+from vgg_relu_layers import VGG_ReLu_Layer
 
 
 class FPL():
