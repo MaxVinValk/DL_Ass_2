fixed fp loss calculation

cnn/fpl.py
@@ -9,6 +9,7 @@ from tensorflow.keras import layers
 from cnn.vgg_relu_layers import VGG_ReLu_Layer
 
 
+
 class FPL():
     """Feature Perceptual Loss
        Contains the function calculate_fp_loss
@@ -86,13 +87,14 @@ class FPL():
         fp_losses = []
         # For every model, caculate the feature perceptual loss
         for idx, model in enumerate(self.models):
-            prediction_1 = model.predict(
-                img1 if idx == 0 else prediction_1, batch_size=self.batch_size)
-            prediction_2 = model.predict(
-                img2 if idx == 0 else prediction_2, batch_size=self.batch_size)
+            prediction_1 = model(
+                img1 if idx == 0 else prediction_1)
+            prediction_2 = model(
+                img2 if idx == 0 else prediction_2)
             mse = tf.keras.losses.MeanSquaredError(reduction='auto')
             pixel_loss.append(mse(prediction_1, prediction_2))
-            fp_losses.append(np.sum(pixel_loss[idx]))
+
+            fp_losses.append(tf.reduce_sum(pixel_loss[idx]))
 
             # # Extra print statements
             # print('prediction shape', prediciton_1.shape)

main.py
@@ -53,12 +53,14 @@ class VAE(keras.Model):
         self.reconstruction_loss_tracker = keras.metrics.Mean(
             name="reconstruction_loss")
         self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
+        self.fp_loss_tracker = keras.metrics.Mean(name="fp_loss")
 
     @property
     def metrics(self):
         return [
             self.total_loss_tracker,
             self.reconstruction_loss_tracker,
+            self.fp_loss_tracker,
             self.kl_loss_tracker
         ]
 
@@ -71,22 +73,24 @@ class VAE(keras.Model):
                 tf.reduce_sum(keras.losses.binary_crossentropy(
                     data, reconstruction), axis=(1, 2))
             )
-            fpl_loss = self.fpl.calculate_fp_loss(data, reconstruction)
+            fp_loss = self.fpl.calculate_fp_loss(data, reconstruction)
             kl_loss = -0.5 * (1 + z_log_var -
                               tf.square(z_mean) - tf.exp(z_log_var))
             kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
             # total_loss = reconstruction_loss + kl_loss
-            total_loss = kl_loss + fpl_loss
+            total_loss = kl_loss + fp_loss
 
             grads = tape.gradient(total_loss, self.trainable_weights)
             self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
             self.total_loss_tracker.update_state(total_loss)
             self.reconstruction_loss_tracker.update_state(reconstruction_loss)
             self.kl_loss_tracker.update_state(kl_loss)
+            self.fp_loss_tracker.update_state(fp_loss)
 
             return {
                 "loss": self.total_loss_tracker.result(),
                 "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+                "fp_loss": self.fp_loss_tracker.result(),
                 "kl_loss": self.kl_loss_tracker.result()
             }
 
@@ -216,7 +220,7 @@ def create_decoder(latent_dim, pre_flatten_shape):
     x = layers.Conv2D(128, (3, 3))(x)
     x = ReplicationPadding2D(padding=(1, 1))(x)
     x = layers.BatchNormalization()(x)
-    decoder_outputs = layers.Conv2D(3, (1,1))(x)
+    decoder_outputs = layers.Conv2D(3, (1, 1))(x)
 
     dec = keras.Model(latent_inputs, decoder_outputs, name="decoder")
     print(dec.summary())
@@ -345,7 +349,7 @@ if __name__ == '__main__':
         data = load_celeba(DATA_PATH, BATCH_SIZE,
                            (RESIZE_HEIGHT, RESIZE_WIDTH))
 
-        train_VAE(vae, data, epochs=2, batch_size=BATCH_SIZE)
+        train_VAE(vae, data, epochs=10, batch_size=BATCH_SIZE)
 
         # encoder.save("enc/")
         # decoder.save("dec/")