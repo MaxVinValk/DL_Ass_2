2282bc013650eafe30fab13b58b800eebfbea0e5
MaxVinValk <max.valk@outlook.com>
2021-04-01 21:22:38 +0200

    Actually hooked up the beta iterator

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import datetime
 from tensorflow import keras
 
 from custom_loss.custom_loss_functions import PaperLoss123, PaperLoss, ReconLoss
+from custom_loss.vgg_relu_layers import VGG_ReLu_Layer
 
 from data.dataset_loader import CelebA
 from vae.vae import VAE
@@ -14,7 +15,7 @@ from util.beta_iterator import BetaIterator
 
 if __name__ == '__main__':
 
-    DATA_PATH = "celeba_cropped_small"
+    DATA_PATH = "celeba_cropped/data"
 
     TIME_STAMP = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     EXP_DIR = f"Experiment_{TIME_STAMP}"
@@ -40,9 +41,9 @@ if __name__ == '__main__':
 
     data = CelebA(DATA_PATH, BATCH_SIZE, (RESIZE_HEIGHT, RESIZE_WIDTH))
 
-    betas = BetaIterator(beta_sum = 150, max_ratio = 5)
+    betaValues = BetaIterator(beta_sum = 150, max_ratio = 3)
 
-    for ratios, betas in iter(betas):
+    for ratios, betas in iter(betaValues):
 
         ratioString = str(ratios).replace("[", "").replace("]", "").replace(",", "").replace(" ", "_")
 
@@ -55,7 +56,10 @@ if __name__ == '__main__':
         architecture = PaperArchitecture(input_shape, latent_dim)
 
         # Swap this out for any of the other loss functions in custom_loss.custom_loss_functions
-        loss_function = PaperLoss123(input_shape, BATCH_SIZE)
+        loss_layers = [VGG_ReLu_Layer.ONE, VGG_ReLu_Layer.TWO, VGG_ReLu_Layer.THREE]
+
+        loss_function = PaperLoss(input_shape, BATCH_SIZE, loss_layers, 1.0, betas)
+        # loss_function = PaperLoss123(input_shape, BATCH_SIZE)
 
         # The optimizer calls the schedule once per train_step = 1 batch,
         # we only want to change the learning rate after a batch, and we want the
@@ -79,5 +83,3 @@ if __name__ == '__main__':
             f.write("DATA:\n\n" + str(data) + "\n\n")
             f.write("ARCHITECTURE:\n\n" + str(architecture) + "\n\n")
             f.write("LOSS FUNCTION:\n\n" + str(loss_function))
-
-
